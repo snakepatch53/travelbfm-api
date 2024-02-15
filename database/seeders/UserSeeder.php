83cf62b7a56a5c88519050e63eeba1cfda08966f
snakepatch53 <snakepatch53@gmail.com>
2024-02-15 16:46:05 -0500

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   database/seeders/UserSeeder.php

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -15,6 +17,18 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+        DB::table("users")
+            ->insert([
+                'name' => "Super",
+                "lastname" => "Administrador",
+                "phone" => "0987654321",
+                "address" => "Calle 123",
+                "state" => "Activo",
+                "role" => "Administrador",
+                'email' => "admin",
+                'password' => Hash::make("admin"),
+                "confirmation_code" => "123456"
+            ]);
         User::factory()
             ->count(2)
             ->create();
