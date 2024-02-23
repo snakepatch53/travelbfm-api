8a882a18c99a60e128bebc8a527496a2e84636bf
snakepatch53 <snakepatch53@gmail.com>
2024-02-22 20:32:53 -0500

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   resources/views/cart-pdf.blade.php

--- resources/views/cart-pdf.blade.php
+++ resources/views/cart-pdf.blade.php
@@ -2,8 +2,6 @@
 <html lang="es">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Cart #{{ $cart['id'] }}</title>
     <style>
         * {
